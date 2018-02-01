979bb29f50c53381eaf3e8ff1f1a8282469f96f9
Nian <268078545@qq.com>
2018-02-01 15:43:06 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,6 +1,8 @@
 sudo apt-get update
 sudo apt-get -y install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip openjdk-8-jdk apache2 python
 
+rm -rf /var/www/html/*
+
 echo 'https://nian0114:admin12051@github.com
 https://imcsg:admin12051@gitee.com' >  ~/.git-credentials
 git config --global credential.helper store
