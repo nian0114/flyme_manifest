5393fdc700d91d02cd802e520302367f0d4d2cb4
Nian <268078545@qq.com>
2018-02-09 21:39:02 +0800

    Update untar.sh

--- untar.sh
+++ untar.sh
@@ -1,19 +1,15 @@
 user=`whoami`
-source_dir="/home/$user/flyme6"
+source_dir="/home/$user/flyme7"
 
 function untar() {
-	rm -rf $source_dir/devices/$1/vendor
 	echo "tar xvf $1-vendor.tar -C $source_dir/devices/$1/"
 	tar xvf $1-vendor.tar -C "$source_dir/devices/$1/"
 	#rm -rf  $1-vendor.tar
 }
 
-untar hero2qlte_free
-untar hero2qlte
-untar herolte_free
-untar herolte
-untar lithium
-untar natrium
+untar heroltexx
 untar nx531j_cm
 untar nx531j_nubia
 
+sed -i -e "s/ro\.product\.model=.*/ro\.product\.model=SM-G9350/g" $source_dir/devices/heroltexx/vendor/system/build.prop
+sed -i -e "s/ro\.product\.device=.*/ro\.product\.device=hero2qlte/g" $source_dir/devices/heroltexx/vendor/system/build.prop
